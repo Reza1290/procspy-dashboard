f17fbe4571b2c1db1842d4bb2f1187b51acc7586
reza <reza.muktasib@gmail.com>
2025-01-28 19:56:24 +0900

    add: dahsboard

---
+++ src/app/dashboard/layout.js
@@ -0,0 +1,10 @@
+export default function DashboardLayout({children}) {
+    return (
+        <section>
+            <div className="max-w-20 bg-white h-screen w-full p-2">
+                
+            </div>
+            {children}
+        </section>
+    )
+}
