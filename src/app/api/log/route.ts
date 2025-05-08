d288edd0a1a73170f59809071325cdd0860736e5
Reza 1290 <reza.muktasib@gmail.com>
2025-05-08 23:51:50 +0700

    fix: no module

--- src/app/api/log/route.ts
+++ src/app/api/log/route.ts
@@ -1,23 +1,23 @@
-// import { NextRequest, NextResponse } from "next/server";
-// import session from "../../../lib/session";
+import { NextRequest, NextResponse } from "next/server";
+import session from "../../../lib/session";
 
 
-// export async function GET(req: NextRequest, res: NextResponse) {
-//     try {
-//         const {searchParams} = new URL(req.url)
-//         const userToken = searchParams.get("userToken")
-//         const token = await session()
-//         const response = await fetch(`${process.env.ENDPOINT}/proctored-user/${userToken}`, {
-//             headers: {
-//                 Authorization: `Bearer ${token}`
-//             }
-//         });
-//         const responseData = await response.json();
-//         return NextResponse.json(responseData);
+export async function GET(req: NextRequest, res: NextResponse) {
+    try {
+        const {searchParams} = new URL(req.url)
+        const userToken = searchParams.get("userToken")
+        const token = await session()
+        const response = await fetch(`${process.env.ENDPOINT}/proctored-user/${userToken}`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+        const responseData = await response.json();
+        return NextResponse.json(responseData);
 
-//     } catch (e) {
-//         console.error(e)
+    } catch (e) {
+        console.error(e)
         
-//         return NextResponse.json({ error: e.message || "Something went wrong" }, { status: 500 })
-//     }
-// }
+        return NextResponse.json({ error: e.message || "Something went wrong" }, { status: 500 })
+    }
+}
