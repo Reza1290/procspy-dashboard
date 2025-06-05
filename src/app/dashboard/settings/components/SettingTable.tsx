8a437c8d119f59cf0bf29115703893a9840b25f3
Reza 1290 <reza.muktasib@gmail.com>
2025-06-06 00:55:12 +0700

    feat: add create var

--- src/app/dashboard/settings/components/SettingTable.tsx
+++ src/app/dashboard/settings/components/SettingTable.tsx
@@ -1,98 +1,170 @@
 "use client"
 import { useEffect, useRef, useState } from "react";
 import { useParams, usePathname } from "next/navigation";
-import { EllipsisVertical, Eye, HistoryIcon, Unplug } from "lucide-react";
+import { CheckCheckIcon, CheckIcon, EllipsisVertical, Eye, HistoryIcon, SaveIcon, Unplug, XIcon } from "lucide-react";
 import session from "../../../../lib/session";
 
 
 export type Setting = {
-    id: string
-    key: string
-    value: string
+  id: string
+  key: string
+  value: string
 };
 
 const SettingTable = () => {
 
-    const [settings, setSettings] = useState<Setting[]>([]);
-    const scrollRef = useRef<HTMLDivElement>(null);
-    const [page, setPage] = useState(1);
-    const [loading, setLoading] = useState(false);
-    const [hasMore, setHasMore] = useState(true);
-    useEffect(() => {
-
-        fetchSettings(1);
-    },[]);
-
-    const fetchSettings = async (nextPage: number) => {
-        try {
-            const token = await session();
-            const res = await fetch(`${process.env.NEXT_PUBLIC_ENDPOINT || 'https://192.168.2.5:5050'}/api/global-settings?page=${nextPage}&paginationLimit=12`, {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-            });
-            const data = await res.json();
-            if (res.ok) {
-                setSettings(prev => {
-                    const newSettings = data.data.filter((d:Setting) => !prev.some(p => p.id === d.id));
-                    return [...prev, ...newSettings];
-                });
-                setHasMore(nextPage < data.totalPages);
-                setLoading(false);
-                setPage(nextPage);
-            }
-        } catch (err) {
-            console.error("Failed to fetch session history", err);
-        }
-    };
+  const [settings, setSettings] = useState<Setting[]>([]);
+  const scrollRef = useRef<HTMLDivElement>(null);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
 
 
-    const handleScroll = () => {
-        const el = scrollRef.current;
-        if (!el || loading || !hasMore) return;
+  const keyRef = useRef<HTMLInputElement>(null)
+  const valRef = useRef<HTMLInputElement>(null)
 
-        if (el.scrollTop + el.clientHeight >= el.scrollHeight - 50) {
-            fetchSettings(page + 1);
-        }
-    };
+  const [addVariableComponentActive, setAddVariableComponentActive] = useState(false)
+
+  useEffect(() => {
+    fetchSettings(1);
+  }, []);
+
+  const fetchSettings = async (nextPage: number) => {
+    try {
+      const token = await session();
+      const res = await fetch(`${process.env.NEXT_PUBLIC_ENDPOINT || 'https://192.168.2.5:5050'}/api/global-settings?page=${nextPage}&paginationLimit=12`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const data = await res.json();
+      if (res.ok) {
+        setSettings(prev => {
+          const newSettings = data.data.filter((d: Setting) => !prev.some(p => p.id === d.id));
+          return [...prev, ...newSettings];
+        });
+        setHasMore(nextPage < data.totalPages);
+        setLoading(false);
+        setPage(nextPage);
+      }
+    } catch (err) {
+      console.error("Failed to fetch session history", err);
+    }
+  };
 
 
-    return (
-        <div className="">
-            <div className="overflow-x-auto border-b border-white/15">
-                <div className="mx-8 my-4">
-                    <div className="flex">
-                      JAWA
-                    </div>
-                </div>
-                <div className="relative max-h-[76vh] overflow-y-auto " onScroll={handleScroll} ref={scrollRef}>
-                    <table className="min-w-full table-fixed">
-                        <thead className="sticky top-0 z-10 backdrop-blur-[2px]">
-                            <tr className="">
-                                <th className="pl-8 pr-4 py-2 text-left font-normal text-slate-100/75 text-sm">Id</th>
-                                <th className="px-4 py-2 text-left font-normal text-slate-100/75 text-sm">Variable Name</th>
-                                <th className="px-4 py-2 text-left font-normal text-slate-100/75 text-sm">Value</th>
-                                <th className="pr-8 pl-4 text-left font-normal text-slate-100/75 text-sm">Action</th>
-                            </tr>
-                        </thead>
-                        <tbody className="">
-                            {settings.map((setting) => (
-                                <tr key={setting.id} className="border-t border-white/10 hover:bg-gray-600/30">
-                                    <td className="pl-8 pr-4 py-4 text-sm text-white/70">{setting.id}</td>
-                                    <td className="px-4 py-4 text-sm font-semibold">{setting.key}</td>
-                                    <td className="px-4 py-4 text-sm text-sky-500/75 font-medium">{setting.value}</td>
-                                    <td className="pr-8 pl-4 py-4 text-xs capitalize flex justify-start items-center gap-4">
-                                        <EllipsisVertical className="max-w-4 aspect-square" />
-                                    </td>
+  const handleScroll = () => {
+    const el = scrollRef.current;
+    if (!el || loading || !hasMore) return;
 
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
+    if (el.scrollTop + el.clientHeight >= el.scrollHeight - 50) {
+      fetchSettings(page + 1);
+    }
+  };
+
+
+  const handleSubmit = async () => {
+    const key = keyRef.current.value
+    const value = keyRef.current.value
+
+    try {
+      const token = await session()
+      const response = await fetch(`${process.env.NEXT_PUBLIC_ENDPOINT || 'https://192.168.2.5:5050'}/api/global-setting`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          key: key,
+          value: value
+        })
+      });
+
+      if (response.ok) {
+        // DO Alert
+      } else {
+
+      }
+    } catch (error) {
+
+    } finally {
+      keyRef.current.value = ""
+      valRef.current.value = ""
+      setAddVariableComponentActive(false)
+    }
+  }
+
+  const closeAddVariableComponent = () => {
+    keyRef.current.value = ""
+    valRef.current.value = ""
+    setAddVariableComponentActive(false)
+  }
+
+  return (
+    <div className="">
+      <div className="overflow-x-auto border-b border-white/15">
+        <div className="mx-8 my-4">
+          <div className="flex justify-between">
+            <div>
+
             </div>
+            <div className="flex gap-4">
+              <button onClick={() => {
+                setAddVariableComponentActive(true)
+              }} className="bg-blue-500 p-2 px-4 text-sm rounded-md min-w-32 hover:bg-blue-600 ">Add Variable</button>
+            </div>
+          </div>
         </div>
-    );
+        <div className="relative max-h-[76vh] overflow-y-auto " onScroll={handleScroll} ref={scrollRef}>
+          <table className="min-w-full table-fixed">
+            <thead className="sticky top-0 z-10 backdrop-blur-[2px]">
+              <tr className="">
+                <th className="pl-8 pr-4 py-2 text-left font-normal text-slate-100/75 text-sm">Id</th>
+                <th className="px-4 py-2 text-left font-normal text-slate-100/75 text-sm">Variable Name</th>
+                <th className="px-4 py-2 text-left font-normal text-slate-100/75 text-sm">Value</th>
+                <th className="pr-8 pl-4 text-left font-normal text-slate-100/75 text-sm">Action</th>
+              </tr>
+            </thead>
+            <tbody className="">
+              {
+                addVariableComponentActive && (
+
+                  <tr key="Uniq" className="border-t border-white/10 hover:bg-gray-600/30">
+                    <td className="pl-8 pr-4 py-4 text-sm text-white/70">-</td>
+                    <td className="px-4 py-2 text-sm font-light">
+                      <input type="text" ref={keyRef} className="p-1 bg-gray-400/10 rounded-md border border-white/10" />
+                    </td>
+                    <td className="px-4 py-2 text-sm font-light">
+                      <input type="text" ref={valRef} id="var-value" className="p-1 bg-gray-400/10 rounded-md border border-white/10" />
+                    </td>
+                    <td className="pr-8 pl-4 py-4 text-xs capitalize flex justify-start items-center gap-4">
+                      <div className="bg-blue-500 w-max rounded p-1 px-2 cursor-pointer flex gap-1 items-center" onClick={handleSubmit}>
+                        <CheckIcon className="w-4" /></div>
+                      <div className="bg-red-500 w-max rounded p-1 px-2 cursor-pointer flex gap-1 items-center" onClick={closeAddVariableComponent}>
+                        <XIcon className="w-4" /></div>
+                    </td>
+
+                  </tr>
+                )
+              }
+              {settings.map((setting) => (
+                <tr key={setting.id} className="border-t border-white/10 hover:bg-gray-600/30">
+                  <td className="pl-8 pr-4 py-4 text-sm text-white/70">{setting.id}</td>
+                  <td className="px-4 py-4 text-sm font-semibold">{setting.key}</td>
+                  <td className="px-4 py-4 text-sm text-sky-500/75 font-medium">{setting.value}</td>
+                  <td className="pr-8 pl-4 py-4 text-xs capitalize flex justify-start items-center gap-4">
+                    <EllipsisVertical className="max-w-4 aspect-square" />
+                  </td>
+
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SettingTable;
