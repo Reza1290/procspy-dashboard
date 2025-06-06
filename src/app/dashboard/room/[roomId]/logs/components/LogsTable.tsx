437834214701b9a59295f0013f6128635ccb1c2d
Reza 1290 <reza.muktasib@gmail.com>
2025-06-06 16:29:32 +0700

    fix: rendering logs

--- src/app/dashboard/room/[roomId]/logs/components/LogsTable.tsx
+++ src/app/dashboard/room/[roomId]/logs/components/LogsTable.tsx
@@ -1,10 +1,10 @@
 "use client";
-import { useEffect, useRef, useState } from "react";
+import { memo, useEffect, useRef, useState } from "react";
 import { useParams, usePathname } from "next/navigation";
 import session from "../../../../../../lib/session";
 import { EllipsisVertical, Eye, InfoIcon, Unplug } from "lucide-react";
 import { formattedTimestamp } from "../../../../../utils/timestamp";
-import { useWebRtc } from "../../../../../../context/WebRtcProvider";
+import { Peer, useWebRtc } from "../../../../../../context/WebRtcProvider";
 
 export enum logType {
     System = "System",
@@ -37,33 +37,23 @@ const LogsTable = () => {
     const [hasMore, setHasMore] = useState(true);
     const initialLoadRef = useRef(false);
 
-    const { peers, eventRef } = useWebRtc()
+    const { peers, notificationCount } = useWebRtc()
+
     useEffect(() => {
-        console.log("Peers changed:", peers);
         if (!roomId) return;
+
+
         setlogs([]);
         setPage(1);
         setHasMore(true);
         fetchlogs(1);
-    }, [peers, roomId]);
+
+    }, [peers, roomId])
 
     useEffect(() => {
-        if (!eventRef.current) return;
-
-        const handleEvent = () => {
-            if (!roomId) return;
-            setlogs([]);
-            setPage(1);
-            setHasMore(true);
-            fetchlogs(1);
-        };
-
-        eventRef.current.on("log", handleEvent);
-
-        return () => {
-            eventRef.current?.off("log", handleEvent);
-        };
-    }, [eventRef, roomId]);
+        if (!roomId || notificationCount.length === 0) return;
+        insertNewLogsFromSocket(1)
+    }, [notificationCount]);
 
     const fetchlogs = async (nextPage: number) => {
         setLoading(true);
@@ -84,20 +74,25 @@ const LogsTable = () => {
                 const el = scrollRef.current;
                 const prevScrollHeight = el?.scrollHeight || 0;
 
-                setlogs((prev) => {
+                setlogs(prev => {
                     const uniqueNewLogs = data.data.filter(
                         (d: LogProps) => !prev.some((p) => p._id === d._id)
                     );
-                    return [...uniqueNewLogs, ...prev];
+
+                    if (uniqueNewLogs.length === 0) return prev;
+
+                    return [...prev, ...uniqueNewLogs];
                 });
 
                 setHasMore(nextPage < data.totalPages);
                 setPage(nextPage);
 
                 requestAnimationFrame(() => {
-                    if (el) {
+                    if (el && nextPage > 1) {
                         const newScrollHeight = el.scrollHeight;
                         el.scrollTop = newScrollHeight - prevScrollHeight;
+                    }else{
+                        el.scrollTop = el.scrollHeight
                     }
                 });
             }
@@ -107,6 +102,40 @@ const LogsTable = () => {
             setLoading(false);
         }
     };
+
+    const insertNewLogsFromSocket = async (count: number) => {
+        setLoading(true);
+        try {
+            const token = await session();
+            const res = await fetch(
+                `${process.env.NEXT_PUBLIC_ENDPOINT || "https://192.168.2.5:5050"}/api/logs-in-room/${roomId}?page=1&paginationLimit=${count}`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            );
+            const data = await res.json();
+            if (res.ok) {
+                setlogs(prev => {
+                    const uniqueNewLogs = data.data.filter(
+                        (d: LogProps) => !prev.some((p) => p._id === d._id)
+                    );
+                    if (uniqueNewLogs.length === 0) return prev;
+                    return [...uniqueNewLogs, ...prev];
+                });
+
+                requestAnimationFrame(() => {
+                    const el = scrollRef.current;
+                    if (el) el.scrollTop = el.scrollHeight;
+                });
+            }
+        } catch (err) {
+            console.error("Failed to fetch session history", err);
+        } finally {
+            setLoading(false);
+        }
+    }
 
     const handleScroll = () => {
         const el = scrollRef.current;
@@ -137,51 +166,7 @@ const LogsTable = () => {
                 >
                     <table className="min-w-full table-fixed">
                         <tbody>
-                            {[...logs].reverse().map((log) => (
-                                <tr
-                                    key={log._id}
-                                    className="border-t border-white/10 hover:bg-gray-600/30"
-                                >
-                                    <td className="pl-8 pr-4 py-3 text-xs capitalize text-right text-slate-100/75">
-                                        {formattedTimestamp(log.timestamp)}
-                                    </td>
-                                    <td className="px-4 py-3 text-xs capitalize">
-                                        <div className="bg-red-500 w-min rounded p-1 px-2">
-                                            {log.flag.severity}
-                                        </div>
-                                    </td>
-                                    <td className="px-4 py-3 min-w-min">
-                                        <InfoIcon />
-                                    </td>
-                                    <td className="px-4 py-3 text-xs font-semibold">
-                                        {log.flagKey || "-"}
-                                    </td>
-                                    <td className="px-4 py-3 text-xs ">
-                                        <div className="flex flex-col gap-2">
-                                            <div className="font-medium">{log.flag.label || "-"} {log.attachment.title && <span className="font-normal bg-white/10 border-white/15 rounded px-1 border"> {log.attachment?.title ?? "Unknown"}</span>} {log.attachment.url && <span className="font-light rounded px-1 italic text-sky-500"> {log.attachment?.url ?? "Unknown"}</span>}</div>
-                                            {
-                                                (log.attachment.file) && (
-                                                    <>
-                                                        <div className="flex justify-center items-center rounded-md font-normal bg-white/10 border-white/15 p-2 border max-w-64 aspect-video">
-                                                            <img className="rounded-md" src={`${process.env.STORAGE_ENDPOINT || 'https://192.168.2.5:5050'}` + log.attachment.file} alt="" />
-                                                        </div>
-                                                    </>
-                                                )
-                                            }
-                                        </div>
-                                    </td>
-                                    <td className="pr-8 pl-4 py-3 text-xs capitalize">
-                                        <EllipsisVertical className="max-w-4 aspect-square" />
-                                    </td>
-                                </tr>
-                            ))}
-                            {loading && (
-                                <tr>
-                                    <td colSpan={6} className="text-center py-4 text-sm text-white/60">
-                                        Loading...
-                                    </td>
-                                </tr>
-                            )}
+                            {[...logs].reverse().map((log) => <BodyTable key={log._id} log={log} />)}
                         </tbody>
                     </table>
                 </div>
@@ -191,3 +176,43 @@ const LogsTable = () => {
 };
 
 export default LogsTable;
+
+
+const BodyTable = memo(function BodyTable({ log }: { log: LogProps }) {
+    return <tr
+        key={log._id}
+        className="border-t border-white/10 hover:bg-gray-600/30"
+    >
+        <td className="pl-8 pr-4 py-3 text-xs capitalize text-right text-slate-100/75">
+            {formattedTimestamp(log.timestamp)}
+        </td>
+        <td className="px-4 py-3 text-xs capitalize">
+            <div className="bg-red-500 w-min rounded p-1 px-2">
+                {log.flag.severity}
+            </div>
+        </td>
+        <td className="px-4 py-3 min-w-min">
+            <InfoIcon />
+        </td>
+        <td className="px-4 py-3 text-xs font-semibold">
+            {log.flagKey || "-"}
+        </td>
+        <td className="px-4 py-3 text-xs ">
+            <div className="flex flex-col gap-2">
+                <div className="font-medium">{log.flag.label || "-"} {log.attachment.title && <span className="font-normal bg-white/10 border-white/15 rounded px-1 border"> {log.attachment?.title ?? "Unknown"}</span>} {log.attachment.url && <span className="font-light rounded px-1 italic text-sky-500"> {log.attachment?.url ?? "Unknown"}</span>}</div>
+                {
+                    (log.attachment.file) && (
+                        <>
+                            <div className="flex justify-center items-center rounded-md font-normal bg-white/10 border-white/15 p-2 border max-w-64 aspect-video">
+                                <img className="rounded-md" src={`${process.env.STORAGE_ENDPOINT || 'https://192.168.2.5:5050'}` + log.attachment.file} alt="" />
+                            </div>
+                        </>
+                    )
+                }
+            </div>
+        </td>
+        <td className="pr-8 pl-4 py-3 text-xs capitalize">
+            <EllipsisVertical className="max-w-4 aspect-square" />
+        </td>
+    </tr>
+})
